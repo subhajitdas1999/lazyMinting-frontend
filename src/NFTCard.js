79ced31d66a132f512b5cc705c2e0e45fbd04668
Subhajit Das <sd651846@gmail.com>
2022-05-24 19:14:39 +0530

    made some code optimization

--- src/NFTCard.js
+++ src/NFTCard.js
@@ -2,50 +2,82 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "./images/default.jpg";
 const NFTCard = (props) => {
-  const [NftIPFSDetails, setNftIPFSDetails] = useState({});
+  const [nftCompleteData, setNFTCompleteData] = useState({db:props});
   useEffect(() => {
     fetch(props.NFTArt.artLink)
       .then((response) => response.json())
-      .then((data) => setNftIPFSDetails(data))
+      .then((data) => {
+        setNFTCompleteData((prev) => {
+          return {
+            ...prev,
+            ipfs: data,
+          };
+        });
+      })
       .catch((err) => {
         alert(err.message);
       });
   }, []);
   return (
-      
-      <Link to={`/nft/${props.NFTArt.tokenId}`} state={props}>
     <div className="nftCard">
-      <div className="cardImage">
-        {NftIPFSDetails.imageURI ? (
-          <img
-            src={`https://ipfs.moralis.io:2053/ipfs/${
-              NftIPFSDetails.imageURI.split("//")[1]
-            }`}
-            alt="Nft art"
-          />
-        ) : (
-          <img src={defaultImg} alt="default img" />
-        )}
-      </div>
-      <div className="cardDescription">
-        <p className="nftArtDescription">{props.NFTArt.NFTArtDescription}</p>
-        {props.NFTArt.isTokenOnchain ? (
-          <p>On chain Token</p>
-        ) : (
-          <p>Off chain Token</p>
-        )}
-        {props.NFTArt.isForSale ? (
-          <div>
-            <span>{props.NFTArt.sellPrice} Ether</span>
+      {nftCompleteData.ipfs ? (
+        <Link
+          to={`/nft/${nftCompleteData.db.NFTArt.tokenId}`}
+          state={nftCompleteData}
+        >
+          <div className="nftCard_loaded">
+            <div className="cardImage" style={{width:"200px", height:"200px"}}>
+              <img
+                src={`https://ipfs.moralis.io:2053/ipfs/${
+                  nftCompleteData.ipfs.imageURI.split("//")[1]
+                }`}
+                alt="Nft art"
+              style={{width:"200px",height:"200px"}}/>
+            </div>
+            <div className="cardDescription">
+              <p className="nftArtDescription">
+                {nftCompleteData.db.NFTArt.NFTArtDescription}
+              </p>
+              {nftCompleteData.db.NFTArt.isTokenOnchain ? (
+                <p>On chain Token</p>
+              ) : (
+                <p>Off chain Token</p>
+              )}
+              {nftCompleteData.db.NFTArt.isForSale ? (
+                <div>
+                  <span>{nftCompleteData.db.NFTArt.sellPrice} Ether</span>
+                </div>
+              ) : (
+                <p>Not for sale yet</p>
+              )}
+            </div>
           </div>
-        ) : (
-          <p>Not for sale yet</p>
-        )}
-
-        
-      </div>
+        </Link>
+      ) : (
+        <div className="nftCard_loaded">
+          <div className="cardImage">
+            <img src={defaultImg} alt="default img" />
+          </div>
+          <div className="cardDescription">
+            <p className="nftArtDescription">
+              {nftCompleteData.db.NFTArt.NFTArtDescription}
+            </p>
+            {nftCompleteData.db.NFTArt.isTokenOnchain ? (
+              <p>On chain Token</p>
+            ) : (
+              <p>Off chain Token</p>
+            )}
+            {nftCompleteData.db.NFTArt.isForSale ? (
+              <div>
+                <span>{nftCompleteData.db.NFTArt.sellPrice} Ether</span>
+              </div>
+            ) : (
+              <p>Not for sale yet</p>
+            )}
+          </div>
+        </div>
+      )}
     </div>
-    </Link>
   );
 };
 
