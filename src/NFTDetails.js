4cd15ce4c90f869b879840050519360fc4f5ab4f
Subhajit Das <sd651846@gmail.com>
2022-05-24 19:14:12 +0530

    handled buying and selling of NFT both offchain and on chain stage1

--- src/NFTDetails.js
+++ src/NFTDetails.js
@@ -1,95 +1,221 @@
 import { useContext, useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { userContext } from "./App";
 import { ABI, RINKEBY_ADDRESS } from "./contracts/MyLazyMinting";
 import { connectToMetamask } from "./utils/ConnectToMetamask";
-import defaultImg from "./images/default.jpg";
 import { ethers } from "ethers";
 import catchAsync from "./utils/catchAsync";
 import LazyMint from "./utils/getNFTSignature";
+import AxiosInstance from "./axoisInstancs";
 
 const NFTDetails = () => {
   const [nftPrice, setNftPrice] = useState("");
+  const [accounts, setAccounts] = useState([]);
   const location = useLocation();
-  const props = location.state;
-  const [user, setUser] = useContext(userContext).userDetails;
-
-  const [NftIPFSDetails, setNftIPFSDetails] = useState({});
-
-  useEffect(() => {
-    fetch(props.NFTArt.artLink)
-      .then((response) => response.json())
-      .then((data) => setNftIPFSDetails(data))
-      .catch((err) => {
-        alert(err.message);
-      });
-  }, []);
+  const nftCompleteData = location.state;
+  const [user] = useContext(userContext).userDetails;
 
   //sell NFT function
+  useEffect(() => {
+    console.log(nftCompleteData);
+    if (nftCompleteData.db.NFTArt.isTokenOnchain) {
+      (async () => {
+        await handleMetamaskConnection();
+      })();
+    }
+  }, []);
 
-  const handleNFTSell = catchAsync(async (e) => {
+  const handleNFTSellChain = catchAsync(async (e) => {
     e.preventDefault();
+
+    // Connect to metamask
     const [provider, accounts, signer] = await connectToMetamask();
 
-    //read mode instance
-    const myLazyMintRead = new ethers.Contract(RINKEBY_ADDRESS, ABI, provider);
-    const myLazyMintWrite = new ethers.Contract(RINKEBY_ADDRESS, ABI, signer);
-    //get the minter role key from contract
-    //check if the address is a minter or not
-    //if not become a minter first
-    if (
-      !(await myLazyMintRead.hasRole(
-        await myLazyMintRead.MINTER_ROLE(),
-        accounts[0]
-      ))
-    ) {
-      const tx = await myLazyMintWrite.SetMinterRole(accounts[0]);
+    //get write mode instance
+    const myLazyMintReadWrite = new ethers.Contract(
+      RINKEBY_ADDRESS,
+      ABI,
+      signer
+    );
+
+    //if the NFT on chain
+    if (nftCompleteData.db.NFTArt.isTokenOnchain) {
+      //if not approved
+
+
+      
+      //  Approve the NFT
+      const approveTx = await myLazyMintReadWrite.setApprovalForAll(
+        myLazyMintReadWrite.address,
+        true
+      );
+
+      //wait for the tx to mint
+      await approveTx.wait();
+    }
+    //if the NFT off chain
+    else {
+      //get the minter role key from contract
+      //check if the address is a minter or not
+      //if not become a minter first
+      if (
+        !(await myLazyMintReadWrite.hasRole(
+          await myLazyMintReadWrite.MINTER_ROLE(),
+          accounts[0]
+        ))
+      ) {
+        const tx = await myLazyMintReadWrite.SetMinterRole(accounts[0]);
+
+        await tx.wait();
+      }
     }
 
     //Format the NFT package
     const NFTPackage = {
-      tokenId: props.NFTArt.tokenId,
-      price: nftPrice,
-      tokenURI: props.NFTArt.tokenURI,
+      tokenId: nftCompleteData.db.NFTArt.tokenId,
+      price: ethers.utils.parseEther(nftPrice.toString()),
+      tokenURI: nftCompleteData.db.NFTArt.tokenURI,
     };
 
     const lazyMint = new LazyMint(RINKEBY_ADDRESS, signer);
-
+    //sign the NFT of the NFT
     const signature = await lazyMint.getSignature(NFTPackage);
 
-    console.log(signature);
+    const res = await AxiosInstance.post("api/nft/sellNFT", {
+      id: nftCompleteData.db.NFTArt._id,
+      signature,
+      price: nftPrice,
+    });
+
+    setNftPrice("");
   });
+
+  //off chain purchasing
+  const handleNFTPurchase = catchAsync(async () => {
+    const [provider, accounts, signer] = await connectToMetamask();
+
+    const myLazyMintReadWrite = new ethers.Contract(
+      RINKEBY_ADDRESS,
+      ABI,
+      signer
+    );
+
+    const NFTPackage = {
+      tokenId: nftCompleteData.db.NFTArt.tokenId,
+      price: ethers.utils.parseEther(
+        nftCompleteData.db.NFTArt.sellPrice.toString()
+      ),
+      tokenURI: nftCompleteData.db.NFTArt.tokenURI,
+    };
+
+    let purchaseTx;
+    //if the token on chain
+    if (nftCompleteData.db.NFTArt.isTokenOnchain) {
+      purchaseTx = await myLazyMintReadWrite.purchaseNFT(
+        NFTPackage,
+        nftCompleteData.db.NFTArt.signatureForNFT,
+        {
+          value: ethers.utils.parseEther(
+            nftCompleteData.db.NFTArt.sellPrice.toString()
+          ),
+        }
+      );
+    }
+    // If the token off chain
+    else {
+      purchaseTx = await myLazyMintReadWrite.claimNFT(
+        NFTPackage,
+        nftCompleteData.db.NFTArt.signatureForNFT,
+        {
+          value: ethers.utils.parseEther(
+            nftCompleteData.db.NFTArt.sellPrice.toString()
+          ),
+        }
+      );
+    }
+
+    //wait for the Tx to be mined
+    await purchaseTx.wait();
+
+    //update in database
+    await AxiosInstance.post("api/nft/buyNFT", {
+      id: nftCompleteData.db.NFTArt._id,
+      ownerAddress: accounts[0],
+    });
+  });
+
+  const handleMetamaskConnection = async () => {
+    const [provider, accounts, signer] = await connectToMetamask();
+    setAccounts(accounts);
+  };
+
+
 
   return (
     <div className="NFTDetails">
       <h1>NFT Details</h1>
+      {console.log("rendered", accounts)}
       <div className="imageContainer">
-        {NftIPFSDetails.imageURI ? (
-          <img
-            src={`https://ipfs.moralis.io:2053/ipfs/${
-              NftIPFSDetails.imageURI.split("//")[1]
-            }`}
-            alt="Nft art"
-          />
-        ) : (
-          <img src={defaultImg} alt="default img" />
-        )}
+        <img
+          src={`https://ipfs.moralis.io:2053/ipfs/${
+            nftCompleteData.ipfs.imageURI.split("//")[1]
+          }`}
+          alt="Nft art"
+          style={{ width: "200px", height: "200px" }}
+        />
       </div>
       <div className="detailsContainer">
-        <h1>{props.NFTArt.NFTArtDescription}</h1>
-        {props.NFTArt.isTokenOnchain ? (
+        <h1>{nftCompleteData.db.NFTArt.artDescription}</h1>
+        {nftCompleteData.db.NFTArt.isTokenOnchain ? (
           //NFT data related to on chain
-          <h1>The NFT is present on chain</h1>
+
+          accounts.length > 0 ? (
+            <div>
+              {accounts[0] === nftCompleteData.db.NFTArt.ownerAddress ? (
+                <div className="sellOnChainNFT">
+                  <h1>I am the owner of this on chain NFT</h1>
+                  <div className="sellNft">
+                    <form onSubmit={handleNFTSellChain}>
+                      <input
+                        type="text"
+                        placeholder="in ether"
+                        value={nftPrice}
+                        onChange={(e) => setNftPrice(e.target.value)}
+                      />
+                      <button type="submit">Sell NFT</button>
+                    </form>
+                  </div>
+                </div>
+              ) : (
+                <div className="buyOnchainNFT">
+                  {nftCompleteData.db.NFTArt.isForSale ? (
+                    <div className="readyToBuyOffChain">
+                      <h2>{nftCompleteData.db.NFTArt.sellPrice} Ether</h2>
+                      <button onClick={handleNFTPurchase}>Buy the NFT</button>
+                    </div>
+                  ) : (
+                    <h2>Not Ready for Buy yet</h2>
+                  )}
+                </div>
+              )}
+            </div>
+          ) : (
+            <button onClick={handleMetamaskConnection}>
+              Connect to metamask to Buy or Sell
+            </button>
+          )
         ) : //NFT data related to off chain
-        user._id === props.NFTArt.user ? (
+        user._id === nftCompleteData.db.NFTArt.user ? (
           //if owner of the NFt
-          <div className="sellTheNFT">
+          <div className="sellTheNFTOffChain">
             <h1>I am the owner if this NFT</h1>
+
             <div className="sellNft">
-              <form onSubmit={handleNFTSell}>
+              <form onSubmit={handleNFTSellChain}>
                 <input
                   type="text"
                   placeholder="in ether"
+                  value={nftPrice}
                   onChange={(e) => setNftPrice(e.target.value)}
                 />
                 <button type="submit">Sell NFT</button>
@@ -98,7 +224,17 @@ const NFTDetails = () => {
           </div>
         ) : (
           // not the owner
-          <h1>I am not owner</h1>
+          <div className="buyTheNftOffChain">
+            <h1>This is off chain NFT</h1>
+            {nftCompleteData.db.NFTArt.isForSale ? (
+              <div className="readyToBuyOffChain">
+                <h2>{nftCompleteData.db.NFTArt.sellPrice} Ether</h2>
+                <button onClick={handleNFTPurchase}>claim the NFT</button>
+              </div>
+            ) : (
+              <h2>Not Ready for Buy yet!!!!</h2>
+            )}
+          </div>
         )}
       </div>
     </div>
