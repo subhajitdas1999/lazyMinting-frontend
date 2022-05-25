4c159e417054939ff93cdf8724067e86daff660a
Subhajit Das <sd651846@gmail.com>
2022-05-25 19:15:00 +0530

    added conditions to handle error from various end

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,6 +1,24 @@
 const catchAsync = (fn) => {
   return (e) => {
-    fn(e).catch((err) => console.log(err));
+    fn(e).catch((err) => {
+      // console.log(err);
+      //api error from backend
+      if(err.name === "AxiosError"){
+        alert(err.response.data.message);
+
+      }
+      //error from contract
+      else if (err.code === "UNPREDICTABLE_GAS_LIMIT") {
+        alert(err.error.message);
+      }
+      //metamask error
+      else if (err.code) {
+        alert(err.message);
+      } else {
+        alert(err.code)
+      }
+      // console.log(err);
+    });
   };
 };
 
