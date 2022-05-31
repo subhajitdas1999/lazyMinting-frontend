58aca03dd293943a7d0d50eff139a2f925ac2a95
Subhajit Das <sd651846@gmail.com>
2022-05-31 12:11:18 +0530

    added NFT small card

--- src/NFTCard.js
+++ src/NFTCard.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaEthereum } from "react-icons/fa";
 import defaultImg from "./images/default.jpg";
 const NFTCard = (props) => {
-  const [nftCompleteData, setNFTCompleteData] = useState({db:props});
+  const [nftCompleteData, setNFTCompleteData] = useState({ db: props });
   useEffect(() => {
     fetch(props.NFTArt.artLink)
       .then((response) => response.json())
@@ -19,48 +20,51 @@ const NFTCard = (props) => {
       });
   }, []);
   return (
-    <div className="nftCard">
+    <div className="w-72 m-5">
       {nftCompleteData.ipfs ? (
         <Link
           to={`/nft/${nftCompleteData.db.NFTArt.tokenId}`}
           state={nftCompleteData}
         >
           <div className="nftCard_loaded">
-            <div className="cardImage" style={{width:"200px", height:"200px"}}>
+            <div className="cardImage h-72">
               <img
                 src={`https://ipfs.moralis.io:2053/ipfs/${
                   nftCompleteData.ipfs.imageURI.split("//")[1]
                 }`}
                 alt="Nft art"
-              style={{width:"200px",height:"200px"}}/>
+                style={{ width: "100%", height: "100%" ,objectFit:"cover",borderRadius:"0.3rem"}}
+              />
             </div>
-            <div className="cardDescription">
-              <p className="nftArtDescription">
-                {nftCompleteData.db.NFTArt.NFTArtDescription}
-              </p>
+            <div className="cardDescription border-2 rounded border-slate-200 pb-2">
+              <p className="nftArtName font-bold">{nftCompleteData.db.NFTArt.artName}</p>
               {nftCompleteData.db.NFTArt.isTokenOnchain ? (
-                <p>On chain Token</p>
+                <p>On Chain</p>
               ) : (
-                <p>Off chain Token</p>
+                <div>
+                  <p className="">Off Chain</p>
+                </div>
               )}
               {nftCompleteData.db.NFTArt.isForSale ? (
                 <div>
-                  <span>{nftCompleteData.db.NFTArt.sellPrice} Ether</span>
+                  <p className=" flex items-center justify-end font-medium text-xl">
+                    {nftCompleteData.db.NFTArt.sellPrice} <FaEthereum />
+                  </p>
                 </div>
               ) : (
-                <p>Not for sale yet</p>
+                <p className=" w-fit mx-auto border-b-2 uppercase border-black">Not for sale yet</p>
               )}
             </div>
           </div>
         </Link>
       ) : (
-        <div className="nftCard_loaded">
+        <div className="nft_notLoaded">
           <div className="cardImage">
             <img src={defaultImg} alt="default img" />
           </div>
           <div className="cardDescription">
             <p className="nftArtDescription">
-              {nftCompleteData.db.NFTArt.NFTArtDescription}
+              {nftCompleteData.db.NFTArt.artDescription}
             </p>
             {nftCompleteData.db.NFTArt.isTokenOnchain ? (
               <p>On chain Token</p>
