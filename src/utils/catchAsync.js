4b76d58c7769cfe429df455f5bce66b1440d3332
Subhajit Das <sd651846@gmail.com>
2022-05-31 12:15:04 +0530

    added global error catching func

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,23 +1,24 @@
-const catchAsync = (fn) => {
+const catchAsync = (fn, handleModalClose) => {
   return (e) => {
     fn(e).catch((err) => {
       // console.log(err);
       //api error from backend
-      if(err.name === "AxiosError"){
+      if (err.name === "AxiosError") {
         alert(err.response.data.message);
-
       }
       //error from contract
       else if (err.code === "UNPREDICTABLE_GAS_LIMIT") {
         alert(err.error.message);
       }
       //metamask error
-      else if (err.code) {
+      else if (err.code && err.message) {
         alert(err.message);
       } else {
-        alert(err.code)
+        alert("something went wrong");
       }
       // console.log(err);
+      // if modal close present close the modal
+      handleModalClose && handleModalClose();
     });
   };
 };
